068db2c7ade1ae2ccbeb18f73d0a393127af334b
Raúl Valverde Sánchez <raulvalverdesanchez@gmail.com>
2018-08-08 14:13:06 +0200

    Capítulo 3 avanzado

--- test.php
+++ test.php
@@ -30,7 +30,33 @@
 	echo $variable2;
 
 	//Cuando queremos hacer un echo de una frase con el valor de una variable en medio utilizamos " "
-	echo "El importe es de $variable_numerica, gracias"
+	echo "El importe es de $variable_numerica, gracias.";
+
+	//PHP asume el tipo adecuado para cada variable en el momeno que se accede
+	$numero = 12345 * 67890;
+	echo substr($numero, 3, 1); //aqui se asume que es un string, se pasa de numero a string
+
+	$pi = "3.1415927";
+	$radio = 5;
+	echo $pi * ($radio * $radio); //se multiplica string por numero, se asume que pi es un numero
+
+	//Definir constantes
+	define("ROOT_LOCATION", "/usr/local/www/");
+	$directory = ROOT_LOCATION;
+	echo $directory;
+
+	//Constantes predefinidas en PHP
+	echo __DIR__; //etc...
+
+	//Print
+	$b = 1;
+	$b ? print "TRUE" : print "FALSE";
+
+	//Funciones
+	function longdate($timestamp) {
+		return date("l F jS Y", $timestamp);
+	}
+	echo longdate(time());
 ?>
 
 <!-- Here's the latest news. -->
